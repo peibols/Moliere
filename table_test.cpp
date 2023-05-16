0fc855ba604baf1c6a601e5ad8999c34243c8660
Daniel Pablos <peibols@narval4.narval.calcul.quebec>
2023-05-16 09:52:23 -0400

    update test

--- table_test.cpp
+++ table_test.cpp
@@ -15,6 +15,23 @@ int main()
   gsl_integration_workspace *wkcm = gsl_integration_workspace_alloc(100000);
   gsl_integration_workspace *wx = gsl_integration_workspace_alloc(100000);
 
+  std::string tables_path="../table_runner/tables/";
+  read_tables(tables_path);
+  use_tables = 1;
+  
+  double pin_min = 4.;
+  double pin_max = 1400.;
+  int pinbins = 500.;
+  double step_pin = (pin_max-pin_min)/double(pinbins);
+  
+  for (int i=0; i<1000; i++) {
+    double pin=pin_min+(pin_max-pin_min)*((double) rand() / (RAND_MAX));
+    std::cout << " p_in= " << pin << std::endl;
+    std::vector<double> parti = gen_particles(pin,0.,0.,21,0.01,wdk,wkcm,wx);
+    std::cout << " Done i= " << i << std::endl;
+  }
+
+  /*
   double pin_min = 4.;
   double pin_max = 1400.;
   int pinbins = 500.;
@@ -31,7 +48,6 @@ int main()
   //for (int i=0; i<1; i++)
   //std::vector<double> parti = gen_particles(500.,0.,0.,21,0.01,wdk,wkcm,wx);  
 
-  /*
   for (int i=400; i<450; i++) {
     //pin = double(i)*step_pin+step_pin/7.;
     pin = double(i)*step_pin+pin_min;
@@ -39,13 +55,7 @@ int main()
     std::cout << std::setprecision(9) << "Num_result= " << num_result << std::endl;
   }
   */
- 
-  std::cout << "Use_tables = " << use_tables << std::endl;
-
-  std::string tables_path="../table_runner/tables/";
-  read_tables(tables_path);
-  use_tables = 1;
-  
+   
   /*
   for (int i=400; i<450; i++) {
     //pin = double(i)*step_pin+step_pin/7.;
@@ -54,13 +64,6 @@ int main()
     std::cout << std::setprecision(9) << "Tab_result= " << std::setprecision(9) << tab_result << std::endl;
   }
   */
-
-  for (int i=0; i<1000; i++) {
-    pin=pin_min+(pin_max-pin_min)*((double) rand() / (RAND_MAX));
-    std::cout << " p_in= " << pin << std::endl;
-    std::vector<double> parti = gen_particles(pin,0.,0.,21,0.01,wdk,wkcm,wx);
-    std::cout << " Done i= " << i << std::endl;
-  }
 
   return 0;
 }
